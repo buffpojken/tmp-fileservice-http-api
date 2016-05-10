66c2e5f085f30c23b5cf8755fe2f43e250cb943c
buffpojken <daniel@sykewarrior.com>
2016-05-10 17:34:46 +0200

    tweaking path

--- filestorage.js
+++ filestorage.js
@@ -57,6 +57,7 @@ class FileManager{
 		const destination = url.parse(data.file); 
 		// This is of course super-temporary, we should verify here that
 		// this file is allowed to be deleted!
+		console.log(destination);
 		return new Promise(function(resolve, reject){
 			fs.unlink(path.join(".", destination.path), function(err){
 				if(err){
