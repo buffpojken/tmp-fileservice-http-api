5921c4fec7be66d6ed301cc8ed231fb2c506a3cb
buffpojken <daniel@sykewarrior.com>
2016-05-10 16:04:54 +0200

    made destionation an explicit path

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParse.urlencoded({
 	extended: true
 }));
 
-var upload = multer({dest: 'uploads'}); 
+var upload = multer({dest: path.join(__dirname, 'uploads')}); 
 
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 
